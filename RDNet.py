420d79b3e4600dad19fd20954fc44045e7e0ab0a
gullirg <51210683+gullirg@users.noreply.github.com>
2019-06-24 13:35:21 +0100

    Update showing contour for bifurcation curve

--- RDNet.py
+++ RDNet.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 21 15:46:25 2019
+
+@author: gulli
+"""
+
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 """
@@ -10,7 +18,7 @@ sometimes called the Erdős-Rényi graph.
 Then check stability using reactive Laplacian eigenvalues.
 """
 __author__ = """Guglielmo Reggio & Shaheim Ogbono-Harmitt"""
-__credits__ = """Aric Hagberg"""
+__credits__ = """Aric Hagberg""" # FOR ERDOS-RENYI NETWORK GENERATION WITH NETWORKX
 
 import networkx as nx
 import numpy as np
@@ -20,15 +28,24 @@ import matplotlib.pyplot as plt
 n=100 # 10 nodes
 m=200 # 20 edges
 
-### RD SYSTEM VARIABLES ###
-#d_u = 0.01
-#d_v = 0.5
+### REACTIVE FUNCTION VARIABLES ###
 a = 1
 b = -2
 c = 2
 d = -2
+
 I = np.identity(n)
-###
+
+### GENERATE RANDOM GRAPH ###
+G=nx.gnm_random_graph(n,m)
+
+### CALCULATE EIGENVALUES OF LAPLACIAN (DIAGONALIZATION) ###
+w,v = np.linalg.eig(nx.laplacian_matrix(G).toarray())
+
+### GENERATE DIAGONALIZED LAPLACIAN ###
+Lambda = np.diag(w)
+
+### CALCULATE STABILITY FOR DIFFERENT DIFFUSIVITIES ###
 x_u= []
 y_v = []
 Phase_Plane = []
@@ -38,14 +55,7 @@ for d_u in np.arange(0, 0.05, 0.001):
     for d_v in np.arange(0, 0.5, 0.01):
         y_v.append(d_v)
 
-        ### GENERATE RANDOM GRAPH ###
-        G=nx.gnm_random_graph(n,m)
-        
-        ### CALCULATE EIGENVALUES OF LAPLACIAN (DIAGONALIZATION) ###
-        w,v = np.linalg.eig(nx.laplacian_matrix(G).toarray())
-        
-        ### DIAGONALIZED LAPLACIAN ###
-        Lambda = np.diag(w)
+
         
         ### REACTIVE LAPLACIAN ###
         A = a * I - d_u * Lambda
@@ -57,38 +67,40 @@ for d_u in np.arange(0, 0.05, 0.001):
                 [A, B], 
                 [C, D]
                 ])
-        
+    
+        ### EIGENVALUES OF REACTIVE LAPLACIAN ###
         y,z = np.linalg.eig(Gamma)
         Gamma_eig = np.real(y)
-
+        
+        ### FIND PERCENTAGE OF POSITIVE EIGENVALUES ###
         check = []
         for g in Gamma_eig:
             if g > 0. :
                 check.append(1)
+            else:
+                check.append(0)
         
-        if not check:
-            temp.append(0)
+        value_norm = sum(check)/len(check) 
+        temp.append(value_norm)
+        
+# ================================================BOOLEAN MATRIX GENERATOR=====
+#         if not check:
+#             temp.append(0)
+#             
+#         else:
+#             temp.append(1)
+# =============================================================================
             
-        else:
-            temp.append(1)
-            
-    
     Phase_Plane.append(temp)
     
-
+### GENERATE TRANSPOSE MATRIX ###
 matrix = np.transpose(np.matrix(Phase_Plane))
 
-
-print(matrix)
-
-print(len(x_u))
-print(len(y_v))
-
-print(matrix.shape)
-
-fig, ax = plt.subplots()
-
-min_val, max_val = 0, len(temp)
-
-
-ax.matshow(matrix, cmap=plt.cm.Blues, origin = 'lower')
+### CONTOUR PLOT ###
+x = np.arange(0, 50, 1)
+y = np.arange(0, 50, 1)
+xx, yy = np.meshgrid(x, y, sparse=True)
+z = matrix
+h = plt.contourf(x,y,z, cmap = plt.cm.bone)
+plt.colorbar()
+plt.show()
